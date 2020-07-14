684f225f21d7c72a3e73398187d5c93f2d621117
Piyush <piyushkhemka123@gmail.com>
2020-07-14 01:01:29 -0700

    added gandhi bot code

--- config.py
+++ config.py
@@ -1,16 +1,9 @@
 import json
 
-config_old_user = json.load(open('old_user_config.json'))
-config_new_user = json.load(open('new_user_config.json'))
+config = json.load(open('config.json'))
 
-username_old=config_old_user['username']
-password_old=config_old_user['password']
-client_id_old = config_old_user['client_id']
-client_secret_old = config_old_user['client_secret']
-user_agent_old = config_old_user['user_agent']
-
-username_new=config_new_user['username']
-password_new=config_new_user['password']
-client_id_new = config_new_user['client_id']
-client_secret_new = config_new_user['client_secret']
-user_agent_new = config_new_user['user_agent']
+username = config['username']
+password = config['password']
+client_id = config['client_id']
+client_secret = config['client_secret']
+user_agent = config['user_agent']
