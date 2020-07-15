20a568d5cbce66e13c02ee4eca7cbf112aafdd8d
Piyush <piyushkhemka123@gmail.com>
2020-07-15 01:11:02 -0700

    final code

--- main.py
+++ main.py
@@ -18,18 +18,19 @@ ghandi = 'ghandi'
 
 insult_conclusion = " The correct spelling is [Gandhi](https://en.wikipedia.org/wiki/Mahatma_Gandhi)."
 
-
-for comment in subreddits.stream.comments():
-    print(comment.body.lower())
+print('Starting my watch. Will occasionally print count of posts I have monitored so far\n')
+for count, comment in enumerate(subreddits.stream.comments()):
+    if count > 0 and count % 1000 == 0:
+        print(f'{count}')
     if ghandi in comment.body.lower():
         try:
             insult_start = random.choice(insults_list)
             comment.reply(insult_start + insult_conclusion)
-            print(f'Posted insult at {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")} \n')
-            time.sleep(100)
+            print(f'Posted insult at {datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}. \n')
+            time.sleep(60)
         except praw.exceptions.APIException as api:
             print(f'Got api exception. Going to sleep for 5 mins \n')
             time.sleep(600)
         except Exception as e:
-            print(f'Exception = {e} \n')
+            print(f'\nException = {e} \n')
             
